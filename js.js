c5fa7fa6d2bd2d220c86c8e116a87241ffc88e6c
luistanachian <53198490+luistanachian@users.noreply.github.com>
2020-03-06 22:57:49 -0300

    html, js, css

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 const o=document.getElementById('o'),px=1,time=1;
-var tp=0,lf=0,key=32,w=119,s=115,d=100,a=97,space=32,width=document.body.offsetWidth,body=document.body,html=document.documentElement,height=Math.max(body.scrollHeight,body.offsetHeight,html.clientHeight,html.scrollHeight,html.offsetHeight);
+var tp=0,lf=0,ohw=10,key=32,w=119,s=115,d=100,a=97,space=32,width=document.body.offsetWidth,body=document.body,html=document.documentElement,height=Math.max(body.scrollHeight,body.offsetHeight,html.clientHeight,html.scrollHeight,html.offsetHeight);
 setInterval(function(){Mover();},time);
 document.addEventListener("keypress", logKey, false);
 function logKey(e) {
@@ -10,27 +10,19 @@ function logKey(e) {
 
 function Mover()
 {
-    if((height-10)>=tp && tp>0){
-        if(key==s)
-            tp+=px;
-        else if(key==w)
-            tp-=px;
+    if((height-ohw)>=tp && tp>0){
+        if(key==s) tp+=px;
+        else if(key==w) tp-=px;
     }
-    else if(tp==0 && key==s)
-        tp+=px;
-    else if((height-10)<=tp && key==w)
-        tp-=px;
+    else if(tp==0 && key==s) tp+=px;
+    else if((height-ohw)<=tp && key==w) tp-=px;
 
-    if((width-10)>=lf && lf>0){
-        if(key==d)
-            lf+=px;
-        else if(key==a)
-            lf-=px;
+    if((width-ohw)>=lf && lf>0){
+        if(key==d) lf+=px;
+        else if(key==a) lf-=px;
     }
-    else if(lf==0 && key==d)
-        lf+=px;
-    else if((width-10)<=lf && key==a) 
-        lf-=px;
+    else if(lf==0 && key==d) lf+=px;
+    else if((width-ohw)<=lf && key==a)  lf-=px;
 
     o.style.top=tp+'px';
     o.style.left=lf+'px';
