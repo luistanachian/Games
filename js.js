3da437fa428603a8b82223cfe66adba5ab671b89
luistanachian <53198490+luistanachian@users.noreply.github.com>
2020-03-06 22:31:17 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,50 +1,48 @@
-const pj=document.getElementById('pj'),pxMov=1;
-var tp=0,lf=0,mov=32,width=document.body.offsetWidth,body=document.body,html=document.documentElement,height=Math.max(body.scrollHeight,body.offsetHeight,html.clientHeight,html.scrollHeight,html.offsetHeight);
+const pj=document.getElementById('pj'),px=1;
+var tp=0,lf=0,key=32,width=document.body.offsetWidth,body=document.body,html=document.documentElement,height=Math.max(body.scrollHeight,body.offsetHeight,html.clientHeight,html.scrollHeight,html.offsetHeight);
 setInterval(function(){Mover();},1);
 document.addEventListener("keypress", logKey, false);
 
 function logKey(event) {
-    var key = event.keyCode;
+    key = event.keyCode;
     if(key == 115 || key == 119 || key == 100 || key == 97  || key == 32) {
-        mov = key;
         Mover();
-        
     }
 }
 
 function Mover()
 {
     if((height - 10) >=  tp && tp > 0){
-        if(mov == 115) {
-            tp += pxMov;
+        if(key == 115) {
+            tp += px;
         }
-        else if(mov == 119) {
-            tp -= pxMov;
+        else if(key == 119) {
+            tp -= px;
         } 
     }
-    else if(tp == 0 && mov == 115) {
-        tp += pxMov;
+    else if(tp == 0 && key == 115) {
+        tp += px;
     }
-    else if((height - 10) <=  tp && mov == 119){
-        tp -= pxMov;
+    else if((height - 10) <=  tp && key == 119){
+        tp -= px;
     }
 
 
 
 
     if((width - 10) >=  lf && lf > 0){
-        if(mov == 100) {
-            lf += pxMov;
+        if(key == 100) {
+            lf += px;
         }
-        else if(mov == 97) {
-            lf -= pxMov;
+        else if(key == 97) {
+            lf -= px;
         }
     }
-    else if(lf == 0 && mov == 100){
-        lf += pxMov;
+    else if(lf == 0 && key == 100){
+        lf += px;
     }
-    else if((width - 10) <=  lf && mov == 97) {
-        lf -= pxMov;
+    else if((width - 10) <=  lf && key == 97) {
+        lf -= px;
     }
 
     pj.style.top = tp + 'px';
